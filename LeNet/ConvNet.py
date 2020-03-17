796c1c49d9c6830d42bfc20c802c7d631ad83dd3
chensheep <chensheep1005@gmail.com>
2020-03-17 20:30:19 +0800

    Explanation of ConvNet.py

--- LeNet/ConvNet.py
+++ LeNet/ConvNet.py
@@ -7,27 +7,54 @@ from OutLayer import *
 class CovNet(object) :
     def __init__(self) :
 
+        # Define convolution core size of Conv3
+        # [[3, 5, 5]] * 6 => [[channel, width, height]] * number_of_convolution_core
+        # By the above, "[[4, 5, 5]] * 9" means that we'll have 9 convolution core 
+        # and size is [4, 5, 5] (channel, width, height).
+        # In Conv3, there are 16 convolution core. See figure 1,2.
         cov3_core_sizes = [[3, 5, 5]] * 6
         cov3_core_sizes.extend([[4, 5, 5]] * 9)
         cov3_core_sizes.extend([[6, 5, 5]])
 
+        # In cov3_mapcombindex, there are 16 lists. 
+        # [0,1,2] means that first convolution core operate on channel 0,1 and 2 
+        # in previous layer and so on. See figure 1,2.
         cov3_mapcombindex = [[0,1,2],[1,2,3],[2,3,4],[3,4,5],[4,5,0],[5,0,1],\
                 [0,1,2,3],[1,2,3,4],[2,3,4,5],[3,4,5,0],[4,5,0,1],[5,0,1,2],[0,1,3,4],[1,2,4,5],[0,2,3,5],[0,1,2,3,4,5]]
 
+        # See figure 1.
+        # Create Conv1
+        # [[28, 28]] * 6 = [[28, 28], [28, 28], [28, 28], [28, 28], [28, 28], [28, 28]]
+        # In this layer, output is 6 feature maps, and each one size is 28 * 28.
+        # [[1, 5, 5]] * 6 = [[1, 5, 5], [1, 5, 5], [1, 5, 5], [1, 5, 5], [1, 5, 5], [1, 5, 5]]
+        # In this layer, there are 6 convolution cores, and each one size is 1 * 5 * 5 (channel * width * height).
         self.covlay1 = CovLayer([[28, 28]] * 6, [[1, 5, 5]] * 6)
+        
+        # Create Pool2
+        # [[14, 14]] * 6 : In this layer, output is 6 feature maps, and each one size is 14 * 14.
+        # [[2, 2]] * 6   : In this layer, there are 6 pooling cores, and each one size is 2 * 2 (width * height).
         self.poolinglay2 = PoolingLayer([[14, 14]] * 6, [[2, 2]] * 6)
         
         self.covlay3 = CovLayer([[10, 10]] * 16, cov3_core_sizes, cov3_mapcombindex)
         self.poolinglay4 = PoolingLayer([[5, 5]] * 16, [[2, 2]] * 16)
         
         self.covlay5 = CovLayer([[1, 1]] * 120, [[16, 5, 5]] * 120)
+        
+        # Create FcLayer6
+        # Input 120 channels, outpur 84 channels.
         self.fclay6 = FcLayer(84, 120)
         
+        # Create OutputLayer7
+        # Input 84 channels, outpur 10 channels.
         self.outputlay7 = OutputLayer(10, 84)
 
+    # Forward Propagation
+    # We train network one picture each time. So shape of mapset (1, 32, 32) (channel, width, height).
     def fw_prop(self, mapset, mapclass = -1) :
         
+        # caculate feature map
         self.covlay1.calc_maps(mapset)
+        # Take output of covlay1 as input of poolinglay2
         self.poolinglay2.calc_maps(self.covlay1.maps)
         self.covlay3.calc_maps(self.poolinglay2.maps, True)
         self.poolinglay4.calc_maps(self.covlay3.maps)
@@ -36,7 +63,10 @@ class CovNet(object) :
         #self.outputlay7.rbf(self.fclay6.maps, mapclass)
         self.outputlay7.rbf_softmax(self.fclay6.maps)
 
+    # Backward Propagation
+    # mapclass : correct answer of mapset
     def bw_prop(self, mapset, mapclass, learn_rate) :
+        # Set correct one value to 1 and other to 0
         output_error = zeros([1, 1, 10])
         output_error[0][0][mapclass] = 1
         
